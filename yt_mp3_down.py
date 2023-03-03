8eb3da2243c0a69efd064d6d139eed32758f08f0
Ruiru Daniel <danielkiguru@gmail.com>
2023-03-03 13:53:02 +0300

    Add files via upload
    First one works, the second one has runtime errors (modules? maybe?)

---
+++ yt_mp3_down.py
@@ -0,0 +1,12 @@
+#pip install pytube
+from pytube import YouTube
+import os
+yt = YouTube(str(input("Enter URL of youtube video: \n ")))
+video = yt.streams.filter(only_audio=True).first()
+print("Enter the destination address (leave blank to save in current directory)")
+destination = str(input(" ")) or '.'
+out_file = video.download(output_path=destination)
+base, ext = os.path.splitext(out_file)
+new_file = base + '.mp3'
+os.rename(out_file, new_file)
+print(yt.title + " has been successfully downloaded.")
